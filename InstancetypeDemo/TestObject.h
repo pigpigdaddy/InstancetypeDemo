0bec7420a8d824e94e98c7f5640e6121b08e66e8
linfengchina <linfeng@linfeng-iOS.local>
2014-09-03 16:58:03 +0800

    Instancetype demo
    Instancetype demo

---
+++ InstancetypeDemo/TestObject.h
@@ -0,0 +1,17 @@
+//
+//  TestObject.h
+//  InstancetypeDemo
+//
+//  Created by pigpigdaddy on 14-9-3.
+//  Copyright (c) 2014年 pigpigdaddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
++ (id)testObjectWithID;
+
++ (instancetype)testObjectWithInstancetype;
+
+@end
---
+++ InstancetypeDemo/TestObject.m
@@ -0,0 +1,23 @@
+//
+//  TestObject.m
+//  InstancetypeDemo
+//
+//  Created by pigpigdaddy on 14-9-3.
+//  Copyright (c) 2014年 pigpigdaddy. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
++ (id)testObjectWithID
+{
+    return [[self alloc] init];
+}
+
++ (instancetype)testObjectWithInstancetype
+{
+    return [[self alloc] init];
+}
+
+@end
